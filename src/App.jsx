9fd4c31a16392e0bfcf7667d4734a6d116dacc21
gohil-gaurav <gohilgaurav89@gmail.com>
2025-04-01 14:19:36 +0530

    8

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
- main
+
 import NavBar2 from './page2/NavBar2'
-=======
+
 import { NavBar } from './page1/NavBar'
- main
+import MyCource from './page2/MyCource'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
- main
-      <NavBar2/>
 
-     <NavBar/>
-main
+     <MyCource/> 
+
+     {/* <NavBar/> */}
+     {/* <NavBar2/> */}
+
     </>
   )
 }
---
+++ src/page2/MyCource.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const MyCource = () => {
+  return (
+    <>
+      <body className='justify-items-center'>
+      <div className="mycourse bg-blue-100 h-150 w-450 mt-10 rounded-4xl">
+        <h2 className='text-3xl pt-10 ml-10 '>My Course</h2>
+        <div className="course bg-amber-200 h-110 w-100 mt-5 ml-15">
+        </div>
+      </div>
+      <div className="quick bg-amber-200 w-450 mt-10">
+        <h1 className='text-4xl'>Quick Access</h1>
+        <h3 className='text-2xl'>Instant access to ypur learning resources</h3>
+        
+
+      </div>
+    </body>
+        </>
+  )
+}
+
+export default MyCource
